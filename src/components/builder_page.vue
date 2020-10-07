64345c4048cdbfb49833d073a993a92433f32df3
Casey Jones <jcaseyj.5@gmail.com>
2020-10-07 16:18:50 -0600

    mostly boiler plate. Added just the beginings of the build page

---
+++ src/components/builder_page.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div id="cpu">
+      <h1>Builder Page</h1>
+      <CPUSs/>
+      <MOBOs/>
+      <Coolers/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CPUSs from "../components/cpus.vue"
+import MOBOs from "../components/motherboards.vue"
+import Coolers from "../components/coolers.vue"
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    CPUSs,
+    MOBOs,
+    Coolers
+  },
+  props: {
+  }
+}
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.component_label {
+  background-color: #4c63af; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/cpus.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <p>
+            <button class="component_label" v-on:click="toggleVisible()">CPUS go here</button>
+            <span v-if="show">
+                <ul>
+                    <li v-for="cpu in theCPUS" :key="cpu.name">
+                        {{cpu.name}}: ${{cpu.price}}
+                    </li>
+                </ul>
+            </span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CPUs",
+    data() {
+        return {
+            theCPUS: [{
+                name: "cpu 1",
+                price: 100
+            },
+            {
+                name: "cpu 2",
+                price: 150
+            }
+            ],
+            show: false
+        }
+    },
+    methods: {
+        toggleVisible() {
+            if (this.show == true) {
+                this.show = false
+            }
+            else {
+                this.show = true
+            }
+        }
+    }
+}
+</script>
